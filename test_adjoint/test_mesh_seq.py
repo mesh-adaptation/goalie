85dfafc5fe0f26c8f661411e39673cbd474bb890
Joe Wallwork <joe.wallwork@outlook.com>
2023-11-28 08:09:59 +0000

    Add tests for h-enrichment

--- test_adjoint/test_mesh_seq.py
+++ test_adjoint/test_mesh_seq.py
@@ -5,7 +5,9 @@ from firedrake import *
 from goalie_adjoint import *
 from goalie.log import *
 from goalie.mesh_seq import MeshSeq
+from goalie.go_mesh_seq import GoalOrientedMeshSeq
 from goalie.time_partition import TimeInterval
+from parameterized import parameterized
 import logging
 import unittest
 from setup_adjoint_tests import *
@@ -126,3 +128,91 @@ class TestGetSolveBlocks(unittest.TestCase):
         with self.assertRaises(ValueError) as cm:
             mesh_seq.get_solve_blocks("field", 0)
         self.assertEqual(str(cm.exception), msg)
+
+
+class TestGlobalEnrichment(unittest.TestCase):
+    """
+    Unit tests for global enrichment of a :class:`GoalOrientedMeshSeq`.
+    """
+
+    def setUp(self):
+        self.field = "field"
+        self.time_interval = TimeInterval(1.0, [1.0], [self.field])
+        self.meshes = [UnitSquareMesh(1, 1)]
+
+    def go_mesh_seq(self, get_function_spaces):
+        return GoalOrientedMeshSeq(
+            self.time_interval,
+            self.meshes,
+            get_function_spaces=get_function_spaces,
+            get_form=empty_get_form,
+            get_bcs=empty_get_bcs,
+            get_solver=empty_get_solver,
+            qoi_type="steady",
+        )
+
+    def get_function_spaces_decorator(self, degree, family, rank):
+        def get_function_spaces(mesh):
+            if rank == 0:
+                return {self.field: FunctionSpace(mesh, degree, family)}
+            elif rank == 1:
+                return {self.field: VectorFunctionSpace(mesh, degree, family)}
+            else:
+                raise NotImplementedError
+
+        return get_function_spaces
+
+    @parameterized.expand([[1], [2]])
+    def test_h_enrichment_mesh(self, num_enrichments):
+        """
+        Base mesh:   1 enrichment:  2 enrichments:
+
+         o-------o     o---o---o      o-o-o-o-o
+         |      /|     |  /|  /|      |/|/|/|/|
+         |     / |     | / | / |      o-o-o-o-o
+         |    /  |     |/  |/  |      |/|/|/|/|
+         |   /   |     o---o---o      o-o-o-o-o
+         |  /    |     |  /|  /|      |/|/|/|/|
+         | /     |     | / | / |      o-o-o-o-o
+         |/      |     |/  |/  |      |/|/|/|/|
+         o-------o     o---o---o      o-o-o-o-o
+        """
+        mesh_seq = self.go_mesh_seq(self.get_function_spaces_decorator("R", 0, 0))
+        mesh_seq_e = mesh_seq.get_enriched_mesh_seq(
+            enrichment_method="h", num_enrichments=num_enrichments
+        )
+        element = mesh_seq.function_spaces[self.field][0].ufl_element()
+        enriched_element = mesh_seq_e.function_spaces[self.field][0].ufl_element()
+        self.assertEqual(mesh_seq[0].num_cells(), 2)
+        self.assertEqual(mesh_seq[0].num_vertices(), 4)
+        self.assertEqual(mesh_seq[0].num_edges(), 5)
+        n = num_enrichments
+        self.assertEqual(mesh_seq_e[0].num_cells(), 2 * 4**n)
+        self.assertEqual(mesh_seq_e[0].num_vertices(), (2 * n + 1) ** 2)
+        self.assertEqual(
+            mesh_seq_e[0].num_edges(),
+            (2**n + 1) * (2 ** (n + 1)) + (2 ** (2 * n)),
+        )
+
+    @parameterized.expand(
+        [
+            ("DG", 0, 0),
+            ("DG", 0, 1),
+            ("CG", 1, 0),
+            ("CG", 1, 1),
+            ("CG", 2, 0),
+            ("CG", 2, 1),
+        ]
+    )
+    def test_h_enrichment_space(self, family, degree, rank):
+        mesh_seq = self.go_mesh_seq(
+            self.get_function_spaces_decorator(family, degree, rank)
+        )
+        mesh_seq_e = mesh_seq.get_enriched_mesh_seq(
+            enrichment_method="h", num_enrichments=1
+        )
+        element = mesh_seq.function_spaces[self.field][0].ufl_element()
+        enriched_element = mesh_seq_e.function_spaces[self.field][0].ufl_element()
+        self.assertEqual(element.family(), enriched_element.family())
+        self.assertEqual(element.degree(), enriched_element.degree())
+        self.assertEqual(element.value_shape, enriched_element.value_shape)
