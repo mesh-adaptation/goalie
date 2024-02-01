fa66501d99528528b77cf6ffe7cb4a59591a8e19
Joe Wallwork <joe.wallwork@outlook.com>
2024-02-01 15:58:17 +0000

    #100: Add point_seq to __init__

--- goalie/__init__.py
+++ goalie/__init__.py
@@ -6,6 +6,7 @@ from goalie.math import *  # noqa
 from goalie.metric import *  # noqa
 from goalie.mesh_seq import *  # noqa
 from goalie.options import *  # noqa
+from goalie.point_seq import *  # noqa
 from goalie.interpolation import *  # noqa
 from goalie.error_estimation import *  # noqa
 
