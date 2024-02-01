414634e77496bd7d815ce51d3a8048f7f254a822
Joe Wallwork <joe.wallwork@outlook.com>
2024-02-01 19:45:34 +0000

    #100: Docstring formatting

--- goalie/mesh_seq.py
+++ goalie/mesh_seq.py
@@ -23,36 +23,28 @@ __all__ = ["MeshSeq"]
 
 class MeshSeq:
     """
-    A sequence of meshes for solving a PDE associated
-    with a particular :class:`~.TimePartition` of the
-    temporal domain.
+    A sequence of meshes for solving a PDE associated with a particular
+    :class:`~.TimePartition` of the temporal domain.
     """
 
-    @PETSc.Log.EventDecorator("goalie.MeshSeq.__init__")
+    @PETSc.Log.EventDecorator()
     def __init__(self, time_partition: TimePartition, initial_meshes: list, **kwargs):
         r"""
-        :arg time_partition: the :class:`~.TimePartition` which
-            partitions the temporal domain
-        :arg initial_meshes: list of meshes corresponding to
-            the subintervals of the :class:`~.TimePartition`,
-            or a single mesh to use for all subintervals
-        :kwarg get_function_spaces: a function, whose only
-            argument is a :class:`~.MeshSeq`, which constructs
-            prognostic
-            :class:`firedrake.functionspaceimpl.FunctionSpace`\s
-            for each subinterval
-        :kwarg get_initial_condition: a function, whose only
-            argument is a :class:`~.MeshSeq`, which specifies
-            initial conditions on the first mesh
-        :kwarg get_form: a function, whose only argument is a
-            :class:`~.MeshSeq`, which returns a function that
-            generates the PDE weak form
-        :kwarg get_solver: a function, whose only argument is
-            a :class:`~.MeshSeq`, which returns a function
-            that integrates initial data over a subinterval
-        :kwarg get_bcs: a function, whose only argument is a
-            :class:`~.MeshSeq`, which returns a function that
-            determines any Dirichlet boundary conditions
+        :arg time_partition: the :class:`~.TimePartition` which partitions the temporal
+            domain
+        :arg initial_meshes: list of meshes corresponding to the subintervals of the
+            :class:`~.TimePartition`, or a single mesh to use for all subintervals
+        :kwarg get_function_spaces: a function, whose only argument is a
+            :class:`~.MeshSeq`, which constructs prognostic
+            :class:`firedrake.functionspaceimpl.FunctionSpace`\s for each subinterval
+        :kwarg get_initial_condition: a function, whose only argument is a
+            :class:`~.MeshSeq`, which specifies initial conditions on the first mesh
+        :kwarg get_form: a function, whose only argument is a :class:`~.MeshSeq`, which
+            returns a function that generates the PDE weak form
+        :kwarg get_solver: a function, whose only argument is a :class:`~.MeshSeq`,
+            which returns a function that integrates initial data over a subinterval
+        :kwarg get_bcs: a function, whose only argument is a :class:`~.MeshSeq`, which
+            returns a function that determines any Dirichlet boundary conditions
         :kwarg parameters: :class:`~.AdaptParameters` instance
         """
         self.time_partition = time_partition
