9d0f6d4aed5eeaf0620665050c94688449a6f4e2
Joe Wallwork <joe.wallwork@outlook.com>
2024-02-29 11:24:02 +0000

    #26: Add function_data to star imports

--- goalie/__init__.py
+++ goalie/__init__.py
@@ -8,6 +8,7 @@ from goalie.mesh_seq import *  # noqa
 from goalie.options import *  # noqa
 from goalie.point_seq import *  # noqa
 from goalie.interpolation import *  # noqa
+from goalie.function_data import *  # noqa
 from goalie.error_estimation import *  # noqa
 
 import numpy as np  # noqa
