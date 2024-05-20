9b54350466a7ae786d2a0dcefdf8f2a2392a89db
ddundo <davord.no@gmail.com>
2024-05-20 12:42:41 +0000

    #175: Use RegEx to tidy up

--- test_adjoint/test_demos.py
+++ test_adjoint/test_demos.py
@@ -4,6 +4,7 @@ Checks that all demo scripts run.
 
 import glob
 import os
+import re
 import shutil
 from os.path import splitext
 
@@ -16,60 +17,22 @@ all_demos = glob.glob(os.path.join(demo_dir, "*.py"))
 # Modifications dictionary to cut down run time of demos:
 # - Each key is the name of a demo script to be modified
 # - Each value is a dictionary where:
-# -- The key is the original string or block of code to be replaced
+# -- The key is the original string or regex pattern that we wish to replace
 # -- The value is the replacement string (use "" to remove the original code)
-gray_scott_block = """
-ic = mesh_seq.get_initial_condition()
-for field, sols in solutions.items():
-    fwd_outfile = VTKFile(f"gray_scott/{field}_forward.pvd")
-    adj_outfile = VTKFile(f"gray_scott/{field}_adjoint.pvd")
-    fwd_outfile.write(*ic[field].subfunctions)
-    for i in range(num_subintervals):
-        for sol in sols["forward"][i]:
-            fwd_outfile.write(*sol.subfunctions)
-        for sol in sols["adjoint"][i]:
-            adj_outfile.write(*sol.subfunctions)
-    adj_end = Function(ic[field]).assign(0.0)
-    adj_outfile.write(*adj_end.subfunctions)
-"""
-gray_scott_split_block = """
-ic = mesh_seq.get_initial_condition()
-for field, sols in solutions.items():
-    fwd_outfile = VTKFile(f"gray_scott_split/{field}_forward.pvd")
-    adj_outfile = VTKFile(f"gray_scott_split/{field}_adjoint.pvd")
-    fwd_outfile.write(ic[field])
-    for i in range(num_subintervals):
-        for sol in sols["forward"][i]:
-            fwd_outfile.write(sol)
-        for sol in sols["adjoint"][i]:
-            adj_outfile.write(sol)
-    adj_outfile.write(Function(ic[field]).assign(0.0))
-"""
-solid_body_rotation_block = """
-for field, sols in solutions.items():
-    fwd_outfile = VTKFile(f"solid_body_rotation/{field}_forward.pvd")
-    adj_outfile = VTKFile(f"solid_body_rotation/{field}_adjoint.pvd")
-    for i in range(len(mesh_seq)):
-        for sol in sols["forward"][i]:
-            fwd_outfile.write(sol)
-        for sol in sols["adjoint"][i]:
-            adj_outfile.write(sol)
-"""
-
 modifications = {
     "burgers-hessian.py": {""""maxiter": 35""": """"maxiter": 3"""},
     "gray_scott.py": {
         "end_time = 2000.0": "end_time = 10.0",
-        gray_scott_block: "",
+        r"ic = mesh_seq.get_initial_condition\(\)\n.*?adj_outfile.write\(\*adj_end.subfunctions\)": "",
     },
     "gray_scott_split.py": {
         "end_time = 2000.0": "end_time = 10.0",
-        gray_scott_split_block: "",
+        r"ic = mesh_seq.get_initial_condition\(\)\n.*?adj_outfile.write\(Function\(ic\[field\]\).assign\(0.0\)\)": "",
     },
     "point_discharge2d-hessian.py": {""""maxiter": 35""": """"maxiter": 3"""},
     "point_discharge2d-goal_oriented.py": {""""maxiter": 35""": """"maxiter": 3"""},
     "solid_body_rotation.py": {
-        solid_body_rotation_block: "",
+        r"for field, sols in solutions.items\(\):\n.*?adj_outfile.write\(sol\)": "",
     },
 }
 
@@ -98,8 +61,8 @@ def test_modifications_original_exists():
         with open(demo_path, "r") as file:
             demo_content = file.read()
             for original in changes.keys():
-                assert (
-                    original in demo_content
+                assert re.search(
+                    original, demo_content, re.DOTALL
                 ), f"Error: '{original}' not found in '{demo_name}'."
 
 
@@ -122,7 +85,9 @@ def test_demos(demo_file, tmpdir, monkeypatch):
     demo_name = os.path.basename(demo_file)
     if demo_name in modifications:
         for original, replacement in modifications[demo_name].items():
-            demo_contents = demo_contents.replace(original, replacement)
+            demo_contents = re.sub(
+                original, replacement, demo_contents, flags=re.DOTALL
+            )
 
     # Execute the modified demo as a standalone script
     context = {
