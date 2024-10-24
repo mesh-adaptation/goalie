1a4334d9bc6deaa19ea9192dd721540451918cd3
acse-ej321 <89605848+acse-ej321@users.noreply.github.com>
2024-10-24 14:09:53 +0100

    merged conflicts for manual edit for rebase

--- goalie/go_mesh_seq.py
+++ goalie/go_mesh_seq.py
@@ -4,6 +4,7 @@ Drivers for goal-oriented error estimation on sequences of meshes.
 
 from collections.abc import Iterable
 from copy import deepcopy
+from copy import deepcopy
 
 import numpy as np
 import ufl
@@ -179,6 +180,12 @@ class GoalOrientedMeshSeq(AdjointMeshSeq):
         # Construct object to hold enriched spaces
         enriched_mesh_seq = type(self)(
             tp,
+        # Create copy of time_partition
+        time_partition = deepcopy(self.time_partition)
+
+        # Construct object to hold enriched spaces
+        enriched_mesh_seq = type(self)(
+            time_partition,
             meshes,
             get_initial_condition=self._get_initial_condition,
             get_solver=self._get_solver,
