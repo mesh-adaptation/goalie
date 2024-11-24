da92d3c0c37e01e65687f2c3ec4edbfce93d8aa6
Joe Wallwork <jw2423@cam.ac.uk>
2024-11-24 10:40:48 +0000

    Refactor MeshSeq unit tests to use base class

--- test/test_mesh_seq.py
+++ test/test_mesh_seq.py
@@ -5,14 +5,106 @@ Testing for the mesh sequence objects.
 import re
 import unittest
 
-from firedrake import Function, FunctionSpace, UnitCubeMesh, UnitSquareMesh
+from firedrake import (
+    Function,
+    FunctionSpace,
+    UnitCubeMesh,
+    UnitIntervalMesh,
+    UnitSquareMesh,
+)
 from parameterized import parameterized
 
 from goalie.mesh_seq import MeshSeq
 from goalie.time_partition import TimeInterval, TimePartition
 
 
-class TestGeneric(unittest.TestCase):
+class BaseClasses:
+    """
+    Base classes for mesh sequence unit testing.
+    """
+
+    class MeshSeqTestCase(unittest.TestCase):
+        """
+        Test case with a simple setUp method and mesh constructor.
+        """
+
+        def setUp(self):
+            self.time_partition = TimePartition(1.0, 2, [0.5, 0.5], ["field"])
+            self.time_interval = TimeInterval(1.0, [0.5], ["field"])
+
+        def trivial_mesh(self, dim):
+            try:
+                return {
+                    1: UnitIntervalMesh(1),
+                    2: UnitSquareMesh(1, 1),
+                    3: UnitCubeMesh(1, 1, 1),
+                }[dim]
+            except KeyError:
+                raise ValueError(f"Dimension {dim} not supported.") from None
+
+
+class TestExceptions(BaseClasses.MeshSeqTestCase):
+    """
+    Unit tests for exceptions raised by :class:`MeshSeq`.
+    """
+
+    def test_inconsistent_dim_error(self):
+        meshes = [self.trivial_mesh(2), self.trivial_mesh(3)]
+        with self.assertRaises(ValueError) as cm:
+            MeshSeq(self.time_partition, meshes)
+        msg = "Meshes must all have the same topological dimension."
+        self.assertEqual(str(cm.exception), msg)
+
+    @parameterized.expand(["get_function_spaces", "get_solver"])
+    def test_notimplemented_error(self, function_name):
+        mesh_seq = MeshSeq(self.time_interval, self.trivial_mesh(2))
+        with self.assertRaises(NotImplementedError) as cm:
+            if function_name == "get_function_spaces":
+                getattr(mesh_seq, function_name)(mesh_seq[0])
+            else:
+                getattr(mesh_seq, function_name)()
+        msg = f"'{function_name}' needs implementing."
+        self.assertEqual(str(cm.exception), msg)
+
+    @parameterized.expand(["get_function_spaces", "get_initial_condition"])
+    def test_return_dict_error(self, method):
+        kwargs = {method: lambda _: 0}
+        with self.assertRaises(AssertionError) as cm:
+            MeshSeq(self.time_interval, self.trivial_mesh(2), **kwargs)
+        msg = f"{method} should return a dict"
+        self.assertEqual(str(cm.exception), msg)
+
+    @parameterized.expand(["get_function_spaces", "get_initial_condition"])
+    def test_missing_field_error(self, method):
+        kwargs = {method: lambda _: {}}
+        with self.assertRaises(AssertionError) as cm:
+            MeshSeq(self.time_interval, self.trivial_mesh(2), **kwargs)
+        msg = "missing fields {'field'} in " + f"{method}"
+        self.assertEqual(str(cm.exception), msg)
+
+    @parameterized.expand(["get_function_spaces", "get_initial_condition"])
+    def test_unexpected_field_error(self, method):
+        kwargs = {method: lambda _: {"field": None, "extra_field": None}}
+        with self.assertRaises(AssertionError) as cm:
+            MeshSeq(self.time_interval, self.trivial_mesh(2), **kwargs)
+        msg = "unexpected fields {'extra_field'} in " + f"{method}"
+        self.assertEqual(str(cm.exception), msg)
+
+    def test_solver_generator_error(self):
+        mesh = self.trivial_mesh(2)
+        f_space = FunctionSpace(mesh, "CG", 1)
+        kwargs = {
+            "get_function_spaces": lambda _: {"field": f_space},
+            "get_initial_condition": lambda _: {"field": Function(f_space)},
+            "get_solver": lambda _: lambda *_: {},
+        }
+        with self.assertRaises(AssertionError) as cm:
+            MeshSeq(self.time_interval, mesh, **kwargs)
+        msg = "solver should yield"
+        self.assertEqual(str(cm.exception), msg)
+
+
+class TestGeneric(BaseClasses.MeshSeqTestCase):
     """
     Generic unit tests for :class:`MeshSeq`.
     """
@@ -29,64 +121,6 @@ class TestGeneric(unittest.TestCase):
         mesh_seq[0] = mesh2
         self.assertEqual(mesh_seq[0], mesh2)
 
-    def test_inconsistent_dim(self):
-        meshes = [UnitSquareMesh(1, 1), UnitCubeMesh(1, 1, 1)]
-        with self.assertRaises(ValueError) as cm:
-            MeshSeq(self.time_partition, meshes)
-        msg = "Meshes must all have the same topological dimension."
-        self.assertEqual(str(cm.exception), msg)
-
-    @parameterized.expand(["get_function_spaces", "get_solver"])
-    def test_notimplemented_error(self, function_name):
-        mesh_seq = MeshSeq(self.time_interval, UnitSquareMesh(1, 1))
-        with self.assertRaises(NotImplementedError) as cm:
-            if function_name == "get_function_spaces":
-                getattr(mesh_seq, function_name)(mesh_seq[0])
-            else:
-                getattr(mesh_seq, function_name)()
-        msg = f"'{function_name}' needs implementing."
-        self.assertEqual(str(cm.exception), msg)
-
-    @parameterized.expand(["get_function_spaces", "get_initial_condition"])
-    def test_return_dict_error(self, method):
-        mesh = UnitSquareMesh(1, 1)
-        kwargs = {method: lambda _: 0}
-        with self.assertRaises(AssertionError) as cm:
-            MeshSeq(self.time_interval, mesh, **kwargs)
-        msg = f"{method} should return a dict"
-        self.assertEqual(str(cm.exception), msg)
-
-    @parameterized.expand(["get_function_spaces", "get_initial_condition"])
-    def test_missing_field_error(self, method):
-        mesh = UnitSquareMesh(1, 1)
-        kwargs = {method: lambda _: {}}
-        with self.assertRaises(AssertionError) as cm:
-            MeshSeq(self.time_interval, mesh, **kwargs)
-        msg = "missing fields {'field'} in " + f"{method}"
-        self.assertEqual(str(cm.exception), msg)
-
-    @parameterized.expand(["get_function_spaces", "get_initial_condition"])
-    def test_unexpected_field_error(self, method):
-        mesh = UnitSquareMesh(1, 1)
-        kwargs = {method: lambda _: {"field": None, "extra_field": None}}
-        with self.assertRaises(AssertionError) as cm:
-            MeshSeq(self.time_interval, mesh, **kwargs)
-        msg = "unexpected fields {'extra_field'} in " + f"{method}"
-        self.assertEqual(str(cm.exception), msg)
-
-    def test_solver_generator_error(self):
-        mesh = UnitSquareMesh(1, 1)
-        f_space = FunctionSpace(mesh, "CG", 1)
-        kwargs = {
-            "get_function_spaces": lambda _: {"field": f_space},
-            "get_initial_condition": lambda _: {"field": Function(f_space)},
-            "get_solver": lambda _: lambda *_: {},
-        }
-        with self.assertRaises(AssertionError) as cm:
-            MeshSeq(self.time_interval, mesh, **kwargs)
-        msg = "solver should yield"
-        self.assertEqual(str(cm.exception), msg)
-
     def test_counting_2d(self):
         mesh_seq = MeshSeq(self.time_interval, [UnitSquareMesh(3, 3)])
         self.assertEqual(mesh_seq.count_elements(), [18])
@@ -98,15 +132,11 @@ class TestGeneric(unittest.TestCase):
         self.assertEqual(mesh_seq.count_vertices(), [64])
 
 
-class TestStringFormatting(unittest.TestCase):
+class TestStringFormatting(BaseClasses.MeshSeqTestCase):
     """
     Test that the :meth:`__str__` and :meth:`__repr__` methods work as intended for
     Goalie's :class:`MeshSeq` object.
     """
-
-    def setUp(self):
-        self.time_partition = TimePartition(1.0, 2, [0.5, 0.5], ["field"])
-        self.time_interval = TimeInterval(1.0, [0.5], ["field"])
 
     def test_mesh_seq_time_interval_str(self):
         mesh_seq = MeshSeq(self.time_interval, [UnitSquareMesh(1, 1)])
