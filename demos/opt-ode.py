91b7c71a68e1eb6059803c47d76a07da84357574
Joe Wallwork <jw2423@cam.ac.uk>
2025-02-09 14:47:05 +0000

    Set up QoI for toy problem

--- demos/opt-ode.py
+++ demos/opt-ode.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from firedrake import *
 
-from goalie import *
+from goalie_adjoint import *
 
 dt = 0.2
 times = np.arange(0, 1.01, dt)
@@ -21,9 +21,9 @@ def get_initial_condition(point_seq):
 
 def get_solver_theta(point_seq):
     def solver(index):
-        tp = point_seq.time_partition
         u, u_ = point_seq.fields["u"]
         R = point_seq.function_spaces["u"][index]
+        tp = point_seq.time_partition
         dt = Function(R).assign(tp.timesteps[index])
         v = TestFunction(R)
 
@@ -32,11 +32,11 @@ def get_solver_theta(point_seq):
         F = (u - u_ - dt * (theta * u + (1 - theta) * u_)) * v * dx
 
         sp = {"ksp_type": "preonly", "pc_type": "jacobi"}
-        dt = tp.timesteps[index]
         t_start, t_end = tp.subintervals[index]
+        dt = tp.timesteps[index]
         t = t_start
         while t < t_end - 1.0e-05:
-            solve(F == 0, u, solver_parameters=sp)
+            solve(F == 0, u, solver_parameters=sp, ad_block_tag="u")
             yield
 
             u_.assign(u)
@@ -45,11 +45,23 @@ def get_solver_theta(point_seq):
     return solver
 
 
-point_seq = PointSeq(
+def get_qoi(point_seq, index):
+    def end_time_qoi():
+        sol = exp(1.0)
+        u = point_seq.fields["u"][0]
+        return (u - sol) * dx
+
+    return end_time_qoi
+
+
+point_seq = AdjointMeshSeq(
     time_partition,
+    VertexOnlyMesh(UnitIntervalMesh(1), [[0.5]]),
     get_function_spaces=get_function_spaces,
     get_initial_condition=get_initial_condition,
     get_solver=get_solver_theta,
+    get_qoi=get_qoi,
+    qoi_type="end_time",
 )
 
 # Initialise control to zero
@@ -57,11 +69,12 @@ point_seq = PointSeq(
 R = point_seq.function_spaces["u"][0]
 point_seq._control = Function(R).assign(0.0)
 
-solutions = point_seq.solve_forward()["u"]["forward"]
+solutions = point_seq.solve_adjoint()
+print(f"QoI: {point_seq.J}")
 
 forward_euler_trajectory = [float(get_initial_condition(point_seq)["u"])]
 forward_euler_trajectory += [
-    float(sol) for subinterval in solutions for sol in subinterval
+    float(sol) for subinterval in solutions["u"]["forward"] for sol in subinterval
 ]
 
 # Plot the trajectory and compare it against the analytical solution. ::
