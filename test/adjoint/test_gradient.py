825217d180ac1c4ed965a97b000d1918ed18612e
Joe Wallwork <jw2423@cam.ac.uk>
2025-02-23 16:22:06 +0000

    Extend to P1 as well as R-space

--- test/adjoint/test_gradient.py
+++ test/adjoint/test_gradient.py
@@ -4,20 +4,17 @@ Testing for gradient computation.
 
 import unittest
 
+import numpy as np
 import ufl
 from firedrake.function import Function
 from firedrake.functionspace import FunctionSpace
-from firedrake.mesh import VertexOnlyMesh
 from firedrake.solving import solve
 from firedrake.ufl_expr import TestFunction
 from firedrake.utility_meshes import UnitIntervalMesh
+from parameterized import parameterized
 
 from goalie.adjoint import AdjointMeshSeq
 from goalie.time_partition import TimeInterval, TimePartition
-
-
-def point_mesh():
-    return VertexOnlyMesh(UnitIntervalMesh(1), [[0.5]])
 
 
 class TestExceptions(unittest.TestCase):
@@ -28,7 +25,7 @@ class TestExceptions(unittest.TestCase):
     def test_attribute_error(self):
         mesh_seq = AdjointMeshSeq(
             TimeInterval(1.0, 1.0, "field"),
-            point_mesh(),
+            UnitIntervalMesh(1),
             qoi_type="steady",
         )
         with self.assertRaises(AttributeError) as cm:
@@ -51,11 +48,15 @@ class TestSingleSubinterval(unittest.TestCase):
     def time_partition(self, dt):
         return TimeInterval(1.0, dt, self.field)
 
-    def test_single_timestep(self):
-        time_partition = self.time_partition(1.0)
-
+    def get_function_space_getter(self, family, degree):
         def get_function_spaces(mesh):
-            return {self.field: FunctionSpace(mesh, "R", 0)}
+            return {self.field: FunctionSpace(mesh, family, degree)}
+
+        return get_function_spaces
+
+    @parameterized.expand([("R", 0), ("CG", 1)])
+    def test_single_timestep(self, family, degree):
+        time_partition = self.time_partition(1.0)
 
         def get_initial_condition(mesh_seq):
             u = Function(mesh_seq.function_spaces[self.field][0])
@@ -90,15 +91,15 @@ class TestSingleSubinterval(unittest.TestCase):
 
         mesh_seq = AdjointMeshSeq(
             time_partition,
-            point_mesh(),
+            UnitIntervalMesh(1),
             get_initial_condition=get_initial_condition,
-            get_function_spaces=get_function_spaces,
+            get_function_spaces=self.get_function_space_getter(family, degree),
             get_solver=get_solver,
             get_qoi=get_qoi,
             qoi_type="steady",
         )
         mesh_seq.solve_adjoint(compute_gradient=True)
-        self.assertAlmostEqual(float(mesh_seq.gradient[0]), 2.0)
+        self.assertTrue(np.allclose(mesh_seq.gradient[0].dat.data, 2.0))
 
     def test_two_timesteps(self):
         raise NotImplementedError("TODO")  # TODO
@@ -114,7 +115,7 @@ class TestTwoSubintervals(unittest.TestCase):
         num_subintervals = 2
         dt = end_time / num_subintervals
         self.time_partition = TimePartition(end_time, num_subintervals, dt, "field")
-        self.meshes = [point_mesh() for _ in range(num_subintervals)]
+        self.meshes = [UnitIntervalMesh(1) for _ in range(num_subintervals)]
 
     def test_single_timestep(self):
         raise NotImplementedError("TODO")  # TODO
