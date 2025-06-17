b1fe3ea44bd3d33959fcdab7688acc2b46d25c34
Joe Wallwork <jw2423@cam.ac.uk>
2025-06-17 08:26:26 +0100

    turbine demo: rename; Taylor test - FIXME

--- demos/optimisation/turbine/fixed_mesh.py
+++ demos/optimisation/turbine/fixed_mesh.py
@@ -16,6 +16,10 @@ mesh_seq = AdjointMeshSeq(
     qoi_type="steady",
 )
 
+# # FIXME: Sub-quadratic convergence
+# # Run a Taylor test to check the gradient is computed correctly
+# mesh_seq.taylor_test("yc")
+
 # Solve the adjoint problem, computing gradients, and plot the x-velocity component of
 # both the forward and adjoint solutions
 solutions = mesh_seq.solve_adjoint(compute_gradient=True)
@@ -23,12 +27,12 @@ u, eta = solutions["solution_2d"]["forward"][0][0].subfunctions
 fig, axes = plt.subplots(figsize=(12, 5))
 axes.set_title(r"Forward $x$-velocity")
 fig.colorbar(tricontourf(u.subfunctions[0], axes=axes, cmap="coolwarm"), ax=axes)
-plt.savefig("opt-turbine_forward.jpg", bbox_inches="tight")
+plt.savefig(f"fixed_mesh_{n}_forward.jpg", bbox_inches="tight")
 fig, axes = plt.subplots(figsize=(12, 5))
 axes.set_title(r"Adjoint $x$-velocity")
 u_star, eta_star = solutions["solution_2d"]["adjoint"][0][0].subfunctions
 fig.colorbar(tricontourf(u_star.subfunctions[0], axes=axes, cmap="coolwarm"), ax=axes)
-plt.savefig("opt-turbine_adjoint.jpg", bbox_inches="tight")
+plt.savefig(f"fixed_mesh_{n}_adjoint.jpg", bbox_inches="tight")
 
 J = mesh_seq.J
 print(f"J = {J:.4e}")
@@ -44,11 +48,11 @@ axes.plot(optimiser.progress["control"], "--x")
 axes.set_xlabel("Iteration")
 axes.set_ylabel("Control")
 axes.grid(True)
-plt.savefig("opt-turbine_control.jpg", bbox_inches="tight")
+plt.savefig(f"fixed_mesh_{n}_control.jpg", bbox_inches="tight")
 
 fig, axes = plt.subplots()
 axes.plot(optimiser.progress["qoi"], "--x")
 axes.set_xlabel("Iteration")
 axes.set_ylabel("QoI")
 axes.grid(True)
-plt.savefig("opt-turbine_qoi.jpg", bbox_inches="tight")
+plt.savefig(f"fixed_mesh_{n}_qoi.jpg", bbox_inches="tight")
